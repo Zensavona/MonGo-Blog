ea360701498372fd098eabcd559a86a96dc17312
Zen Savona <z@zensavona.com>
2012-09-25 09:13:39 +1000

    serving...

--- mblog.go
+++ mblog.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"labix.org/v2/mgo"
 	//"labix.org/v2/mgo/bson"
+    "net/http"
+    "github.com/russross/blackfriday"
+    "github.com/hoisie/mustache"
+    "io/ioutil"
 )
 
 type Note struct {
@@ -13,31 +17,44 @@ type Note struct {
 	Body string
 }
 
+var notes []Note = loadNotes()
+
+// less than ideal
+var home, _ = ioutil.ReadFile("home.md")
+var homeMarkup = string(blackfriday.MarkdownCommon(home))
+
 func loadNotes() ([]Note) {
 	session, err := mgo.Dial("localhost")
     if err != nil {
             panic(err)
     }
     defer session.Close()
-
-    // Optional. Switch the session to a monotonic behavior.
-    session.SetMode(mgo.Monotonic, true)
-
     c := session.DB("test").C("notes")
-
 	notes := []Note{}
     iter := c.Find(nil).Iter()
     err = iter.All(&notes)
     if err != nil {
         panic(iter.Err())
     }
-
     return notes
 }
 
-func main() {
-	notes := loadNotes()
-    for _, note := range notes {
-        fmt.Println(note.Title)
+func loadTemplate(name string) string {
+    file, _ := ioutil.ReadFile(name + ".html")
+    return string(file)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    var data struct {
+        Notes []Note
     }
+    data.Notes = notes
+    rendered := mustache.RenderInLayout(homeMarkup, loadTemplate("home"), data)
+    fmt.Fprintf(w, rendered)
+}
+
+func main() {
+    http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
+    http.HandleFunc("/", indexHandler)
+    http.ListenAndServe(":8080", nil)
 }
